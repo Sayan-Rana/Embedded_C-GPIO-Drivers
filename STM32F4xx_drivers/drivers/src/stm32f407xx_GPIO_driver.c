cffb4aec3e4146b6d7d4191eb4c826eaf676df27
Sayan <rana.sayan@gmail.com>
2020-12-07 03:07:31 +0530

    stm32f407xx_GPIO_driver.c update

--- STM32F4xx_drivers/drivers/src/stm32f407xx_GPIO_driver.c
+++ STM32F4xx_drivers/drivers/src/stm32f407xx_GPIO_driver.c
@@ -378,9 +378,65 @@ void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
  *
  *	@note                - none
  */
-void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t EnOrDis)
+void GPIO_IRQ_IntConfig(uint8_t IRQNumber, uint8_t EnOrDis)
 {
+	// Enabling or disabling Interrupt in NVIC ISER or ICER registers
+	if(EnOrDis == ENABLE)
+	{
+		if(IRQNumber >= 0 && IRQNumber <= 31)
+		{
+			// Program ISER0 register
+			*NVIC_ISER0 |= (1 << IRQNumber);
 
+		}else if(IRQNumber > 31 && IRQNumber <= 63)
+		{
+			// Program ISER1 register
+			*NVIC_ISER1 |= (1 << (IRQNumber % 32));
+		}else if(IRQNumber > 63 && IRQNumber <= 95)
+		{
+			// Program ISER2 register
+			*NVIC_ISER2 |= (1 << (IRQNumber % 32));
+		}
+	}else{
+		if(IRQNumber >= 0 && IRQNumber <= 31)
+		{
+			// Program ICER0 register
+			*NVIC_ICER0 |= (1 << IRQNumber);
+		}else if(IRQNumber > 31 && IRQNumber <= 63)
+		{
+			// Program ICER1 register
+			*NVIC_ICER1 |= (1 << (IRQNumber % 32));
+		}else if(IRQNumber > 63 && IRQNumber <= 95)
+		{
+			// Program ICER2 register
+			*NVIC_ICER2 |= (1 << (IRQNumber % 32));
+		}
+	}
+}
+
+
+/****************************************************************************************
+ *	@fn                  - GPIO_IRQPriorityConfig
+ *
+ *	@brief               - This function will configure the priority of IRQ
+ *
+ *	@param[in]           - IRQ Priority number
+ *	@param[in]           - IRQ number
+ *
+ *	@return              - none
+ *
+ *	@note                - none
+ *
+ */
+void GPIO_IRQPriorityConfig(uint8_t IRQNumber, uint8_t IRQPriority)
+{
+	// 1. Find out IPR Register
+	uint8_t iprx = (IRQNumber / 4);
+	uint8_t iprx_section = (IRQNumber % 4);
+
+	// 2. Configure the IPR Register
+	uint8_t shift_amount = (8 * iprx_section) + (8 - NO_PR_BITS_IMPLEMENTED);
+	*(NVIC_IPR_BASE_ADDR + iprx) |= (IRQPriority << shift_amount);
 }
 
 
@@ -398,6 +454,11 @@ void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t EnOrDis)
  */
 void GPIO_IRQHandling(uint8_t PinNumber)
 {
-
+	// Clear the EXTI PR register by setting it 1 corresponding to the pin number
+	if(EXTI->EXTI_PR & (1 << PinNumber))
+	{
+		// Clear that pending register bit
+		EXTI->EXTI_PR |= (1 << PinNumber);
+	}
 }
 
